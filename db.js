1150483c87e734d4ec0847e2b9716a6a7a046c08
Raj Paladiya <131861282+Rajpaladiya@users.noreply.github.com>
2023-12-16 17:59:57 +0530

    small fixes

--- db.js
+++ db.js
@@ -7,17 +7,4 @@ const MongoToConnect=()=>{
     mongoose.connect(mongoURI)
 }
 
-// const connectToMongoDB = async () => {
-//     try {
-//       await mongoose.connect(process.env.MONGODB_URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       });
-//       console.log('Connected to MongoDB');
-//     } catch (error) {
-//       console.error('Error connecting to MongoDB:', error.message);
-//       process.exit(1); // Exit the process if unable to connect
-//     }
-//   };
-
 module.exports = MongoToConnect;
