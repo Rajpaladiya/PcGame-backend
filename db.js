652c87d3c27573b8fe28dbf5c86cf828272f27b1
Raj Paladiya <131861282+Rajpaladiya@users.noreply.github.com>
2023-12-16 17:55:46 +0530

    small fix

--- db.js
+++ db.js
@@ -3,21 +3,21 @@ const mongoURI = 'mongodb://127.0.0.1:27017/game'
 
 
 
-// const MongoToConnect=()=>{
-//     mongoose.connect(mongoURI)
-// }
+const MongoToConnect=()=>{
+    mongoose.connect(mongoURI)
+}
 
-const connectToMongoDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error.message);
-      process.exit(1); // Exit the process if unable to connect
-    }
-  };
+// const connectToMongoDB = async () => {
+//     try {
+//       await mongoose.connect(process.env.MONGODB_URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//       });
+//       console.log('Connected to MongoDB');
+//     } catch (error) {
+//       console.error('Error connecting to MongoDB:', error.message);
+//       process.exit(1); // Exit the process if unable to connect
+//     }
+//   };
 
-module.exports = connectToMongoDB;
+module.exports = MongoToConnect;
